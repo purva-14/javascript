133c66d8bb2569187fd439e61d934b322d2fa673
purva-14 <140138406+purva-14@users.noreply.github.com>
2026-02-16 18:24:49 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"// treat all JS CODE as newer version
+
+// alert("hello") we use it only in browser
+console.log(3+3);
+
+let name="purva"
+let age=18
+let isLoggedIn=false
+
+//number
+//bigint
+//string
+//boolean
+//null=>empty 
+//undefined=>value is not assigned yet
+//symbol=>unique
+
+//object
+console.log(typeof name)
+
