84e11b5e213911df5d09c94c7bd8f9a634163caf
purva-14 <140138406+purva-14@users.noreply.github.com>
2026-02-18 17:49:12 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,11 @@
+// #Primitive
+//7types:String ,Number,Boolean,Null,Undefined,Symbol,BigInt
+
+const score=100 // js is a dynamic type cuz we dont declare datatypes
+
+
+// #Reference type or non Primitive
+//Arrays,Objects,Functions
+
+//null datatype is always object
+//object datatype is function
