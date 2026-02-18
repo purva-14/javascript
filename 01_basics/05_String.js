7738eb278ace8cc0570c06f58fadfa75d1f949e5
purva-14 <140138406+purva-14@users.noreply.github.com>
2026-02-18 18:19:44 +0000

    Strings

---
+++ 01_basics/05_String.js
@@ -0,0 +1,22 @@
+const name="purva"
+const repoCount=90;
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+const gameName=new String('purva');
+console.log(gameName[1])
+console.log(gameName.__proto__)
+
+
+const newString="          purva"
+console.log(gameName.length)
+console.log(gameName.substring(0,2)) //cant give neg values
+console.log(gameName.slice(-8,4)) //can give neg values
+console.log(gameName.trim(newString)) //removes extra spaces
+
+console.log(gameName.indexOf('t'))
+
+console.log(newString.includes('h'))
+
+
+
