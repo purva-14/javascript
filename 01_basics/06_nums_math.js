2dea7b181aeb6ab41e2d0f7ac501b95aaf469948
purva-14 <140138406+purva-14@users.noreply.github.com>
2026-02-18 18:39:52 +0000

    math and num

---
+++ 01_basics/06_nums_math.js
@@ -0,0 +1,20 @@
+const score=400;
+console.log(score)
+
+const balance=new Number(100);
+console.log(balance)
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otheNumber=123.8966
+console.log(otheNumber.toPrecision(3))
+
+const hundreds=10000000
+console.log(hundreds.toLocaleString()) //add comma according to us standards
+console.log(hundreds.toLocaleString('en-IN')) //add comma according to indian standards
+
+//+++++++++++++++++++++++++Maths++++++++++++++++++++++
+
+console.log(Math.abs(-4));
+console.log(Math.round(4.89));
