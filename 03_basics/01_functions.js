3d51506c6e151f515610652a1f47b03358bc3373
purva-14 <140138406+purva-14@users.noreply.github.com>
2026-02-22 07:24:39 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,15 @@
+function myname(){
+console.log("purva")
+}
+
+myname()
+function addnum(num1,num2){
+    console.log(num1+num2);
+}
+addnum(5,6)
+
+function calCartPrice(...num){
+    return num
+}
+console.log(calCartPrice(200,400,500,780))
+
