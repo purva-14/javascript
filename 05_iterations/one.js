a2211d129be684d4c1b5a047e92634c9e06c74a5
purva-14 <140138406+purva-14@users.noreply.github.com>
2026-02-23 19:01:42 +0000

    iterator

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -15,4 +15,24 @@ while(index<=10){
     index=index+1;
 }
 
-let arr=0;
+// let arr=0;
+//----------------------------------------------------------------
+//for Of
+const arr=[1,2,3,4,5,6,7]
+for(const num of arr){
+    console.log(num);
+}
+
+//Maps
+// const map=new Map()
+// map.set('In' ,"India")
+// map.set('usa',"united states of america")
+// console.log(map)
+
+const myobj={
+    'game':"NFS",
+    'game2':"spiderman"
+}
+for(const key in myobj){
+    console.log(key);
+}
